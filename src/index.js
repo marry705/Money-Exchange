44746b3f6ff39fc3c80f15a6dff58aebea7e40c4
Marry705 <marry705chilo@gmail.com>
2018-09-30 22:19:18 +0300

    spelling correction

--- src/index.js
+++ src/index.js
@@ -2,29 +2,28 @@
 module.exports = function makeExchange(currency = 0) {
 let rez = {};
 
-if (currency<=0){return rez;}
-else {if (currency>10000) {rez={error: "You are rich, my friend! We don't have so much coins for exchange"};
-        }
+if (currency<=0) return rez;
+else {
+      if (currency>10000) rez={error: "You are rich, my friend! We don't have so much coins for exchange"};
       else {
-
-        let H=Math.floor(currency/50);
+        let H = Math.floor(currency/50);
         currency=currency-H*50;   
-        if (H!==0) { rez.H = H;}
+        if (H!==0)  rez.H = H;
         
-        let Q=Math.floor(currency/25);
+        let Q = Math.floor(currency/25);
         currency=currency-Q*25;
-        if (Q!==0) { rez.Q = Q;}
+        if (Q!==0) rez.Q = Q;
         
-        let D=Math.floor(currency/10);
+        let D = Math.floor(currency/10);
         currency=currency-D*10;
-        if (D!==0) {rez.D = D;}
+        if (D!==0) rez.D = D;
         
-        let N=Math.floor(currency/5);
+        let N = Math.floor(currency/5);
         currency=currency-N*5;
-        if (N!==0) {rez.N = N;}
+        if (N!==0) rez.N = N;
         
-        let P=Math.floor(currency/1);
-        if (P!==0) {rez.P = P;}
+        let P = Math.floor(currency/1);
+        if (P!==0) rez.P = P;
       }
 }
 return rez;
