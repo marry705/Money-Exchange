e2d97b1b4b2d792455cc067bb8055efaab4a59df
Marry705 <marry705chilo@gmail.com>
2019-03-03 23:18:17 +0300

    chaged algaritm

--- src/index.js
+++ src/index.js
@@ -2,29 +2,23 @@
 module.exports = function makeExchange(currency = 0) {
 let rez = {};
 
-if (currency<=0) return rez;
-else {
-      if (currency>10000) rez={error: "You are rich, my friend! We don't have so much coins for exchange"};
-      else {
-        let H = Math.floor(currency/50);
-        currency=currency-H*50;   
-        if (H!==0)  rez.H = H;
+  if (currency>10000) rez={error: "You are rich, my friend! We don't have so much coins for exchange"};
+  else {
+    
+    if (Math.floor(currency/50)>0)  rez.H = Math.floor(currency/50);
+    currency=currency-Math.floor(currency/50)*50;  
         
-        let Q = Math.floor(currency/25);
-        currency=currency-Q*25;
-        if (Q!==0) rez.Q = Q;
-        
-        let D = Math.floor(currency/10);
-        currency=currency-D*10;
-        if (D!==0) rez.D = D;
-        
-        let N = Math.floor(currency/5);
-        currency=currency-N*5;
-        if (N!==0) rez.N = N;
-        
-        let P = Math.floor(currency/1);
-        if (P!==0) rez.P = P;
-      }
-}
+    if (Math.floor(currency/25)>0) rez.Q = Math.floor(currency/25);
+    currency=currency-Math.floor(currency/25)*25;
+
+    if (Math.floor(currency/10)>0) rez.D = Math.floor(currency/10);
+    currency=currency-Math.floor(currency/10)*10;
+
+    if (Math.floor(currency/5)>0) rez.N = Math.floor(currency/5);
+    currency=currency-Math.floor(currency/5)*5;
+
+    if (Math.floor(currency/1)>0) rez.P = Math.floor(currency/1);
+  }
+
 return rez;
 }
